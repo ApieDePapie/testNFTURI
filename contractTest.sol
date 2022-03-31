c118c6b9a13c19edece0c6bd5a29aad4375d6b20
ApieDePapie <44950217+ApieDePapie@users.noreply.github.com>
2022-03-31 19:14:47 +0200

    Update contractTest.sol

--- contractTest.sol
+++ contractTest.sol
@@ -1,92 +1,138 @@
-contract Contract {
-    function main() {
-        // Error: StackRead before write???
-        var var-1;
-        memory[0x40:0x60] = var-1;
-        var-1 = msg.value;
-    
-        if (var-1) { revert(memory[0x00:0x00]); }
-    
-        if (msg.data.length < 0x04) { stop(); }
-    
-        var var0 = msg.data[0x00:0x20] >> 0xe0;
-    
-        if (0x70a08231 > var0) {
-            if (0x313ce567 > var0) {
-                if (var-1 == 0x06fdde03) {
-                    // Error: StackRead before write???
-                    var var-2;
-                    // Error: StackRead before write???
-                    var var-3;
-                    var-3 = var-2 + var-3;
-                    // Unhandled termination
-                } else if (var-2 == 0x095ea7b3) {
-                    var-3 = var-2 + var-3;
-                    // Unhandled termination
-                } else if (var-2 == 0x18160ddd) {
-                    var-3 = var-2 + var-3;
-                    // Unhandled termination
-                } else if (var-2 == 0x23b872dd) {
-                    var-3 = var-2 + var-3;
-                    // Unhandled termination
-                } else { revert(memory[0x00:0x00]); }
-            } else if (var-1 == 0x313ce567) {
-                var-3 = byte(var-3, var-1 * var-2);
-                // Error: StackRead before write???
-                var var-5;
-                // Error: StackRead before write???
-                var var-4;
-                var-4 = storage[var-3 i/ var-4] & 0xff;
-                // Error: Could not resolve jump destination!
-            } else if (var-1 == 0x36a7abd3) {
-                // Error: Could not resolve jump destination!
-            } else if (var-1 == 0x5a3b7e42) {
-                var-2 = var-1 + var-2;
-                // Unhandled termination
-            } else if (var-1 == 0x6f2436cc) {
-                memory[0x610858565b60408051:0x610858565b60408071] = var-3 & (0x01 << 0xa0) - 0x01;
-                var temp0 = memory[var-1 * var-2:var-1 * var-2 + 0x20];
-                return memory[temp0:temp0 + 0x610858565b60408051 - temp0 + 0x20];
-            } else { stop(); }
-        } else if (0xa9059cbb > var0) {
-            if (var0 == 0x70a08231) {
-                // Dispatch table entry for balanceOf(address)
-                var-2 = var-1 + var-2;
-                // Unhandled termination
-            } else if (var-1 == 0x79ba5097) {
-                // Error: Could not resolve jump destination!
-            } else if (var-1 == 0x8da5cb5b) {
-                var-2 = var-1 * var-2;
-                var-1 = 0x61094b565b61010d61;
-                // Unhandled termination
-            } else if (var-1 == 0x95d89b41) {
-                var-2 = var-1 + var-2;
-                // Unhandled termination
-            } else { stop(); }
-        } else if (var0 == 0xa9059cbb) {
-            // Dispatch table entry for transfer(address,uint256)
-            var-1 = var0 + var-1;
-            // Unhandled termination
-        } else if (var0 == 0xd4ee1d90) {
-            // Dispatch table entry for newOwner()
-            var temp1 = 0x610aba565b6101ca60 / (var0 * var-1);
-            var-1 = temp1;
-            var0 = msg.data.length - var-1;
-        
-            if (var0 < 0x40) { revert(memory[0x00:0x00]); }
-        
-            var temp2 = (0x01 << 0xa0) - 0x01;
-            var temp3 = var0;
-            var0 = temp2 & msg.data[temp3:temp3 + 0x20];
-            var var1 = msg.data[temp3 + 0x20:temp3 + 0x20 + 0x20] & temp2;
-            // Unhandled termination
-        } else if (var0 == 0xdd62ed3e) {
-            // Dispatch table entry for allowance(address,address)
-            var-1 = var0 + var-1;
-            // Unhandled termination
-        } else if (var0 == 0xf2fde38b) {
-            // Dispatch table entry for transferOwnership(address)
-            // Error: Could not resolve jump destination!
-        } else { stop(); }
-    }
-}
+# Palkeoramix decompiler. 
+
+def storage:
+  owner is addr at storage 0
+  newOwner is addr at storage 1
+  standard is array of uint256 at storage 2
+  name is array of uint256 at storage 3
+  symbol is array of uint256 at storage 4
+  decimals is uint8 at storage 5
+  totalSupply is uint256 at storage 6
+  unknown6f2436ccAddress is addr at storage 7
+  balanceOf is mapping of uint256 at storage 8
+  allowance is mapping of uint256 at storage 9
+
+def name() payable: 
+  return name[0 len name.length]
+
+def totalSupply() payable: 
+  return totalSupply
+
+def decimals() payable: 
+  return decimals
+
+def standard() payable: 
+  return standard[0 len standard.length]
+
+def unknown6f2436cc() payable: 
+  return unknown6f2436ccAddress
+
+def balanceOf(address _owner) payable: 
+  require calldata.size - 4 >= 32
+  return balanceOf[_owner]
+
+def owner() payable: 
+  return owner
+
+def symbol() payable: 
+  return symbol[0 len symbol.length]
+
+def newOwner() payable: 
+  return newOwner
+
+def allowance(address _owner, address _spender) payable: 
+  require calldata.size - 4 >= 64
+  return allowance[_owner][_spender]
+
+#
+#  Regular functions
+#
+
+def _fallback() payable: # default function
+  revert
+
+def unknown36a7abd3(addr _param1) payable: 
+  require calldata.size - 4 >= 32
+  require caller == owner
+  unknown6f2436ccAddress = _param1
+  log 0xbd1294ae: _param1
+
+def transferOwnership(address _newOwner) payable: 
+  require calldata.size - 4 >= 32
+  require caller == owner
+  if owner == _newOwner:
+      revert with 0, 'New owner is equal to owner.'
+  newOwner = _newOwner
+
+def acceptOwnership() payable: 
+  if newOwner != caller:
+      revert with 0, 'Sender is not new owner.'
+  log OwnerUpdate(
+        address prevOwner=owner,
+        address newOwner=newOwner)
+  owner = newOwner
+  newOwner = 0
+
+def transfer(address _to, uint256 _value) payable: 
+  require calldata.size - 4 >= 64
+  if not _to:
+      revert with 0, 'ENJ: _address is 0.'
+  if _value > balanceOf[caller]:
+      revert with 0, 'sub underflow'
+  balanceOf[caller] -= _value
+  if _value + balanceOf[_to] < balanceOf[_to]:
+      revert with 0, 'add overflow'
+  balanceOf[addr(_to)] = _value + balanceOf[_to]
+  log Transfer(
+        address from=_value,
+        address to=caller,
+        uint256 tokens=_to)
+  return 1
+
+def approve(address _spender, uint256 _value) payable: 
+  require calldata.size - 4 >= 64
+  if not _spender:
+      revert with 0, 'ENJ: _address is 0.'
+  if unknown6f2436ccAddress == _spender:
+      revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                  32,
+                  36,
+                  0xfe454e4a3a2063727970746f4974656d7320697320616c726561647920617070726f7665,
+                  mem[200 len 28]
+  if _value:
+      if allowance[caller][addr(_spender)]:
+          revert with 0, 'ENJ: Current allowance is not 0.'
+  allowance[caller][addr(_spender)] = _value
+  log Approval(
+        address tokenOwner=_value,
+        address spender=caller,
+        uint256 tokens=_spender)
+  return 1
+
+def transferFrom(address _from, address _to, uint256 _value) payable: 
+  require calldata.size - 4 >= 96
+  if not _from:
+      revert with 0, 'ENJ: _address is 0.'
+  if not _to:
+      revert with 0, 'ENJ: _address is 0.'
+  if unknown6f2436ccAddress != caller:
+      if _value > allowance[addr(_from)][caller]:
+          revert with 0, 'sub underflow'
+      allowance[addr(_from)][caller] -= _value
+  if _value > balanceOf[addr(_from)]:
+      revert with 0, 'sub underflow'
+  balanceOf[addr(_from)] -= _value
+  if _value + balanceOf[_to] < balanceOf[_to]:
+      revert with 0, 'add overflow'
+  balanceOf[addr(_to)] = _value + balanceOf[_to]
+  log Approval(
+        address tokenOwner=allowance[addr(_from)][caller],
+        address spender=_from,
+        uint256 tokens=caller)
+  log Transfer(
+        address from=_value,
+        address to=_from,
+        uint256 tokens=_to)
+  return 1
+
+
